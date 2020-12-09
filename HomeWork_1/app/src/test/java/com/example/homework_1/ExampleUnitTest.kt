66262a804baf37e0e88da0b1b001dcbdb5e90553
hyperman98 <32795542+hyperman98@users.noreply.github.com>
2020-12-09 23:19:52 +0500

    Add files via upload

--- HomeWork_1/app/src/test/java/com/example/homework_1/ExampleUnitTest.kt
+++ HomeWork_1/app/src/test/java/com/example/homework_1/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
-package com.example.homework_1
+package com.example.napoleonitapp
 
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,18 +12,57 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
 
-    val listOfDevelopers = listOf<DataClasses>(
-        DataClasses("Nikolas", 22, 5),
-        DataClasses("Artyom", 21, 7),
-        DataClasses("Kirill", 20, 8),
-        DataClasses("Maxim", 19, 9)
-    )
-
-    fun List<DataClasses>.sort():List<DataClasses> {
-        return this.sortedBy { it.skillRating }
-    }
     @Test
-    fun addition_isCorrect() {
-        println(listOfDevelopers.sort())
+    fun start() {
+
+        val eventDataClass1 = EventDataClass("Человек-паук", "22.05", "300")
+        val eventDataClass2 = EventDataClass("Звездные войны", "24.04", "400")
+        val eventDataClass3 = EventDataClass("Крик", "10.01", "350")
+        val eventDataClass4 = EventDataClass("Бой с тенью", "01.01", "500")
+        val eventDataClass5 = EventDataClass("Боец", "15.10", "600")
+        var eventCollection = mutableListOf<EventDataClass>(
+            eventDataClass1,
+            eventDataClass2,
+            eventDataClass3,
+            eventDataClass4,
+            eventDataClass5
+        )
+        eventCollection.sort(1)
+        println("Сортировка по фильму: ")
+        println()
+
+        for (i in 0..4){
+            println("Фильм: " + eventCollection[i].film + "; Дата: " + eventCollection[i].date + "; Количество: "+ eventCollection[i].price)
+        }
+
+        println()
+        eventCollection = eventCollection.sort(2).toMutableList()
+        println("Сортировка по дате: ")
+        println()
+
+        for (i in 0..4){
+            println("Фильм: " + eventCollection[i].film + "; Дата: " + eventCollection[i].date + "; Количество: "+ eventCollection[i].price)
+        }
+
+        println()
+        eventCollection = eventCollection.sort(3).toMutableList()
+        println("Сортировка по цена: ")
+        println()
+
+        for (i in 0..4){
+            println("Название: " + eventCollection[i].film + "; Дата: " + eventCollection[i].date + "; Количество: "+ eventCollection[i].price)
+        }
+
+    }
+
+    fun MutableList<EventDataClass>.sort(type: Int): MutableList<EventDataClass> {
+
+
+        when (type) {
+            1 -> this.sortBy { it.film }
+            2 ->  this.sortBy { it.date }
+            3 ->  this.sortBy { it.price }
+        }
+        return this;
     }
 }
